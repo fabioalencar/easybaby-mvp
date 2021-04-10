06e815da5ed0eed9655f1dce80cb205911b90779
byfabioalencar@gmail.com <byfabioalencar@gmail.com>
2021-04-10 19:12:33 -0300

    Adding _app.js

---
+++ provider/pages/_app.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import "../styles/index.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head></Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
