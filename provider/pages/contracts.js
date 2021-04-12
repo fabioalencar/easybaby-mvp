ed00abfe35d2327c79a26d06b26524bfa14d4665
rafaelxbs <rafaxbs@gmail.com>
2021-04-11 22:45:25 -0300

    Alterações de 11-4

--- provider/pages/contracts.js
+++ provider/pages/contracts.js
@@ -2,11 +2,29 @@ import getConfig from 'next/config';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { parseCookies } from 'nookies';
+import Router from 'next/router';
 
-function Contracts({ authData, contracts }) {
+function Contracts({ authData, contracts}) {
+
+  const handleNewForm = (authData, contract) => {
+    
+
+    if (true) {
+      Router.push('/newContract');
+    }
+  };
+
+  const handleOpenForm = (authData, contract) => {
+    
+
+    if (true) {
+      Router.push('/contractForm');
+    }
+  };
+
   const { user } = authData;
-
-  return (
+  console.log('AQUI! '+JSON.stringify(contracts));
+  return ( //TO-DO: formatar essa tabela!
     <div className="flex flex-col h-screen justify-between bg-gray-100">
       <header className=" px-2 sm:px-4 lg:px-4 bg-white shadow mb-8">
         <Navbar user={user} />
@@ -19,14 +37,49 @@ function Contracts({ authData, contracts }) {
         </div>
       </header>
       <main className="max-w-5xl mx-auto px-4 p-20 sm:px-6 lg:px-8 bg-white">
+      <table>
+        <tr>
+          <th>Nome do contrato |</th><th>Nome do plano |</th><th>Nome do paciente</th>
+        </tr>
+      </table>
         {contracts.map((contract) => (
-          <div key={contract.id}>{contract.NAME}</div>
+          <>
+          <table>
+            <tr>
+            <td>
+            <button //className="w-full bg-primary-100 hover:bg-primary-50 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
+            onClick={() => handleOpenForm(authData, contract)}
+            >
+            <div key={contract.id}>{contract.name}|</div>
+            </button>
+            
+            </td>
+            
+            <td><div key={contract.id}>{contract.plan.TITLE}|</div></td>
+            <td><div key={contract.id}>{contract.customer.NAME}</div></td>
+            </tr>
+            
+          </table>
+
+          </>
         ))}
+        <table>
+        <tr><td><div className="flex w-full mt-8">
+              <button
+                className="w-full bg-primary-100 hover:bg-primary-50 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
+                type="button"
+                onClick={() => handleNewForm(authData, contracts)}
+              >
+                Novo contrato
+              </button></div></td></tr>
+        </table>
       </main>
       <Footer />
     </div>
   );
 }
+
+
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -34,7 +87,7 @@ export async function getServerSideProps(ctx) {
   const cookie = parseCookies(ctx).user;
   const userData = cookie != undefined ? JSON.parse(cookie) : false;
 
-  const res = await fetch(`${publicRuntimeConfig.STRAPI_API_URL}/maternities`, {
+  const res = await fetch(`${publicRuntimeConfig.STRAPI_API_URL}/contracts`, {
     headers: {
       Authorization: `Bearer ${userData.jwt}`,
     },
